8e8fdc48f014d28244832c55e1170306faac6b73
inkiu0 <inkiu0@gmail.com>
2018-01-23 14:48:43 +0800

    提交optimal_group

--- ExcelTools/Scripts/Lua/loptimal.cs
+++ ExcelTools/Scripts/Lua/loptimal.cs
@@ -60,7 +60,7 @@ namespace Lua
         public static void optimal(string path, string outpath)
         {
             table table = lparser.parse(path);
-            Setting ts = GetTableSetting(table);
+            Setting ts = get_table_setting(table);
             string ret = string.Empty;
             switch(ts.type)
             {
@@ -68,7 +68,7 @@ namespace Lua
                     ret = optimal_normal(table);
                     break;
                 case Optimization.Group:
-                    ret = optimal_group(table);
+                    ret = optimal_group(table, ts);
                     break;
                 case Optimization.Skill:
                     break;
@@ -81,7 +81,7 @@ namespace Lua
         }
 
         #region Normal优化，即只抽取公共字段，将重复次数最多的值作为默认值
-        static Setting GetTableSetting(table table)
+        static Setting get_table_setting(table table)
         {
             Setting type = _NormalSetting;
             if (_OptimalSetting.ContainsKey(table.name))
@@ -92,14 +92,14 @@ namespace Lua
         /// <summary>
         /// 以LuaTable的第一条配置的所有属性作为base
         /// </summary>
-        /// <param name="table"></param>
+        /// <param name="configs"></param>
         /// <returns></returns>
-        static Dictionary<string, Dictionary<string, int>> GetBaseProperties(table table)
+        static Dictionary<string, Dictionary<string, int>> get_base_properties(List<config> configs)
         {
             Dictionary<string, Dictionary<string, int>> basedic = new Dictionary<string, Dictionary<string, int>>();
-            if (table.configs.Count > 0)
+            if (configs.Count > 0)
             {
-                config baseconfig = table.configs[0];
+                config baseconfig = configs[0];
                 for (int i = 0; i < baseconfig.properties.Count; i++)
                 {
                     basedic.Add(baseconfig.properties[i].name, new Dictionary<string, int>{ { baseconfig.properties[i].value, 1 } });
@@ -108,7 +108,7 @@ namespace Lua
             return basedic;
         }
 
-        static Dictionary<string, string> CountBaseKVs(Dictionary<string, Dictionary<string, int>> basedic)
+        static Dictionary<string, string> count_baseKVs(Dictionary<string, Dictionary<string, int>> basedic)
         {
             Dictionary<string, string> basekv = new Dictionary<string, string>();
             string val = string.Empty;
@@ -129,7 +129,7 @@ namespace Lua
             return basekv;
         }
 
-        static void SwapPropertiesBase(Dictionary<string, Dictionary<string, int>> o, Dictionary<string, Dictionary<string, int>> n)
+        static void swap_properties_base(Dictionary<string, Dictionary<string, int>> o, Dictionary<string, Dictionary<string, int>> n)
         {
             Dictionary<string, Dictionary<string, int>> temp = o;
             o = n;
@@ -164,17 +164,17 @@ namespace Lua
             return table.GenString(genmeta);
         }
 
-        static string optimal_normal(table table)
+        static Dictionary<string, string> get_basekvs(List<config> configs)
         {
-            Dictionary<string, Dictionary<string, int>> basedic = GetBaseProperties(table);
+            Dictionary<string, Dictionary<string, int>> basedic = get_base_properties(configs);
             Dictionary<string, Dictionary<string, int>> newbase = new Dictionary<string, Dictionary<string, int>>();
             property temp;
             //configs在构造时即为 new List<string>()，一定不为空
-            for(int i = 0; i < table.configs.Count; i++)
+            for (int i = 0; i < configs.Count; i++)
             {
-                for(int j = 0; j < table.configs[i].properties.Count; j++)
+                for (int j = 0; j < configs[i].properties.Count; j++)
                 {
-                    temp = table.configs[i].properties[j];
+                    temp = configs[i].properties[j];
                     if (basedic.ContainsKey(temp.name))
                     {
                         newbase.Add(temp.name, basedic[temp.name]);
@@ -184,29 +184,33 @@ namespace Lua
                             newbase[temp.name].Add(temp.value, 1);
                     }
                 }
-                SwapPropertiesBase(basedic, newbase);
+                swap_properties_base(basedic, newbase);
             }
-            return gen_normal(table, CountBaseKVs(basedic));
+            return count_baseKVs(basedic);
+        }
+
+        static string optimal_normal(table table)
+        {
+            return gen_normal(table, get_basekvs(table.configs));
         }
         #endregion
 
         #region Group优化，即分组优化，自动将特征值一致的配置分为同一组。然后抽取相同的值，作为元表。
 
-        static void partition_grop(table table, List<string> eigenvalues)
+        static Dictionary<string, List<config>> partition_grop(table table, List<string> eigenvalues)
         {
-            Dictionary<string, List<string>> group = new Dictionary<string, List<string>>();
-            Dictionary<string, int> dic = new Dictionary<string, int>();
-            for (int i = 0; i < eigenvalues.Count; i++)
-                dic.Add(eigenvalues[i], i);
-            int total = dic.Count;
+            Dictionary<string, List<config>> group = new Dictionary<string, List<config>>();
+            int total = eigenvalues.Count;
             string[] strs = new string[total];
+            int idx;
             for(int i = 0; i < table.configs.Count; i++)
             {
                 for(int j = 0; j < table.configs[i].properties.Count; j++)
                 {
-                    if(dic.ContainsKey(table.configs[i].properties[i].name))
+                    idx = eigenvalues.IndexOf(table.configs[i].properties[i].name);
+                    if(idx > -1)
                     {
-                        strs[dic[table.configs[i].properties[i].name]] = table.configs[i].properties[i].value;
+                        strs[idx] = table.configs[i].properties[i].value;
                         total--;
                     }
                 }
@@ -214,16 +218,118 @@ namespace Lua
                 {
                     string key = string.Join(string.Empty, strs);
                     if (group.ContainsKey(key))
-                        group[key].Add(table.configs[i].key);
+                        group[key].Add(table.configs[i]);
                     else
-                        group.Add(key, new List<string> { table.configs[i].key });
+                        group.Add(key, new List<config> { table.configs[i] });
                 }
-                total = dic.Count;
+                total = eigenvalues.Count;
             }
+            return group;
         }
 
-        static string optimal_group(table table)
+        /// <summary>
+        /// key为一组中第一个被记录下的那个值，使用这个值便于使用时查找。
+        /// </summary>
+        /// <param name="basekvs"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        static string gen_base_metatable(Dictionary<string, string> basekvs, string key)
         {
+            StringBuilder sb = new StringBuilder(string.Format("[{0}] = {", key));
+            int n = basekvs.Count;
+            foreach(var item in basekvs)
+            {
+                n--;
+                sb.AppendFormat("{0} = {1}", item.Key, item.Value);
+                if (n > 0)
+                    sb.Append(", ");
+            }
+            sb.Append("}");
+            return sb.ToString();
+        }
+
+        static string gen_group_metatable(List<config> configs)
+        {
+            StringBuilder sb = new StringBuilder("{");
+            for(int i = 0; i < configs.Count; i++)
+            {
+                sb.Append(configs[i].key);
+                if (i < configs.Count - 1)
+                    sb.Append(", ");
+            }
+            sb.Append("}");
+            return sb.ToString();
+        }
+
+        static string gen_group(table table, Dictionary<string, List<config>> group)
+        {
+            Dictionary<string, Dictionary<string, string>> basedic = new Dictionary<string, Dictionary<string, string>>();
+            Dictionary<List<config>, Dictionary<string, string>> groupdic = new Dictionary<List<config>, Dictionary<string, string>>();
+            foreach (var list in group.Values)
+            {
+                if (list.Count > 0)
+                {
+                    Dictionary<string, string> basekvs = get_basekvs(list);
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        if (basedic.ContainsKey(list[i].key))
+                            Console.Error.WriteLine("已存在的key = " + list[i].key);
+                        else
+                            basedic.Add(list[i].key, basekvs);
+                    }
+                    groupdic.Add(list, basekvs);
+                }
+            }
+
+            //去除不需要生成的属性
+            property temp;
+            Dictionary<string, string> basekv = new Dictionary<string, string>();
+            for (int i = 0; i < table.configs.Count; i++)
+            {
+                if (group.ContainsKey(table.configs[i].key))
+                {
+                    basekv = basedic[table.configs[i].key];
+                    for (int j = 0; j < table.configs[i].properties.Count; j++)
+                    {
+                        temp = table.configs[i].properties[j];
+                        if (basekv.ContainsKey(temp.name) && basekv[temp.name] == temp.value)
+                            table.configs.RemoveAt(j);
+                    }
+                }
+            }
+
+            Func<string> genmeta = () =>
+            {
+                StringBuilder __base = new StringBuilder("local __base = {\n");
+                StringBuilder __groups = new StringBuilder("local groups = {\n");
+                int n = groupdic.Count;
+                foreach (var item in groupdic)
+                {
+                    n--;
+                    __groups.Append(gen_group_metatable(item.Key));
+                    __base.Append(gen_base_metatable(item.Value, item.Key[0].key));
+                    if(n > 0)
+                    {
+                        __groups.Append(",\n");
+                        __base.Append(",\n");
+                    }
+                    else
+                    {
+                        __groups.Append("\n");
+                        __base.Append("\n");
+                    }
+                }
+                __base.Append("for _,v in pairs(__base) do\n\tv.__index = v\nend\n");
+                __groups.Append("for i=1, #groups do\n\tfor j=1, #groups[i] do\n\t\tsetmetatable(%s[groups[i][j]], __base[groups[i][1]])\n\tend\nend\n");
+                return __base.ToString() + __groups.ToString();
+            };
+            return table.GenString(genmeta);
+        }
+
+        static string optimal_group(table table, Setting setting)
+        {
+            Dictionary<string, List<config>> group = partition_grop(table, setting.EigenValues);
+
             return string.Empty;
         }
         #endregion
